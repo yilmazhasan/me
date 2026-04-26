beaa8aa8e04e2792fe9e83373c9213d69966e955
yilmazhasan <yilmazhasan111@gmail.com>
2026-04-26 16:59:20 +0300

    Delete CourseRegister app

--- docs/personal/works/CourseRegister/user/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION["username"]);
-   unset($_SESSION["password"]);
-   
-//   echo 'You have cleaned session';
-   header('Refresh: 0; URL = login.php');
-?>
